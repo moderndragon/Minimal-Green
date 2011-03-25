dac9425c48043ea0e2eacd2c762c171aee6c0fbc
ryuu <thursa@riesenheim.net>
2011-03-25 21:37:48 +0100

    initial commit
    modified colors. added background for header.

--- header.php
+++ header.php
@@ -18,8 +18,3 @@
 	<h1 id="blog-title"><span><a href="<?php bloginfo('home') ?>/" title="<?php echo wp_specialchars( get_bloginfo('name'), 1 ) ?>" rel="home"><?php bloginfo('name') ?></a></span></h1>
 		<div id="blog-description"><?php bloginfo('description') ?></div>
 	</div><!--  #header -->
-
-	<div id="access">
-		<!-- <div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div> -->
-		<?php sandbox_globalnav() ?>
-	</div> <!-- #access -->
