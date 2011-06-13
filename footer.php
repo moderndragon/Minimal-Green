8d647509bec8bb5f545c8daece545a318b48aebf
ryuu <thursa@riesenheim.net>
2011-06-13 15:42:10 +0200

    furnished footer.php with project link

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 
 	<div id="footer">
-		<span id="generator-link"> Dieses Blog <!-- hier Home-Link & Blogtite einfügen --> l&auml;uft mit <a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
+		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
 		<span class="meta-sep">|</span>
-		Das Theme von <span id="developer-link"><a href="http://ryuu.de">ryuu</a></span> basiert auf der <span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox-Theme f&uuml;r WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
+		<a href="http://ryuu.de/projekte/minimal-green">minimal green theme</a> by <a href="http://ryuu.de">ryuu</a> based on the <span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
 	</div><!-- #footer -->
 
 </div><!-- #wrapper .hfeed -->
