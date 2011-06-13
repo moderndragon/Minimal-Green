b59d1d2818221fa271fb5db74435ff974264e073
ryuu <thursa@riesenheim.net>
2011-06-13 15:44:00 +0200

    Wordpress credits in footer changed

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 
 	<div id="footer">
-		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
+		This blog is powered by <span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
 		<span class="meta-sep">|</span>
 		<a href="http://ryuu.de/projekte/minimal-green">minimal green theme</a> by <a href="http://ryuu.de">ryuu</a> based on the <span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
 	</div><!-- #footer -->
